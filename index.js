9614593925470e7027856b40eba0eacfb49de1c5
ShulgaVlad <113896377+ShulgaVlad@users.noreply.github.com>
2025-10-15 15:47:04 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+exports.httpEntry = (req, res) => {
+    res.set('Access-Control-Allow-Origin', '*');
+    res.set('Access-Control-Allow-Headers', 'Content-Type');
+    res.set('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
+    if (req.method === 'OPTIONS') return res.status(204).send('');
+    const name = req.query.name || (req.body && req.body.name) || 'world';
+    res.status(200).json({
+        hello: name,
+        runtime: 'nodejs',
+        region: process.env.GCP_REGION || 'unknown'
+    });
+};
