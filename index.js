4161e95b451b8171c65f65ea2e2846f00e12a221
ShulgaVlad <113896377+ShulgaVlad@users.noreply.github.com>
2025-10-15 20:29:47 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,32 @@
 const http = require('http');
+const url = require('url');
 
 const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  
-  res.end('Hello from Render!\n');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+
+    if (req.method === 'OPTIONS') {
+        res.writeHead(204); // Статус 204 No Content
+        res.end();
+        return;
+    }
+
+    const parsedUrl = url.parse(req.url, true);
+    const name = parsedUrl.query.name || 'world';
+
+    const responseObject = {
+        hello: name,
+        runtime: 'nodejs',
+        region: process.env.GCP_REGION || 'unknown'
+    };
+    
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify(responseObject));
 });
 
 server.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+    console.log(`Server running on port ${port}`);
 });
